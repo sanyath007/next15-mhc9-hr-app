9d4bb756b6d2b7440d905da9861a7effae6b04d3
sanyath007 <sanyath007@gmail.com>
2025-11-22 07:06:17 +0700

    build: update next.config.ts file

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,14 @@
 import type { NextConfig } from "next";
+import { PHASE_DEVELOPMENT_SERVER } from 'next/constants'
+
+// const isDev =  phase === PHASE_DEVELOPMENT_SERVER;
 
 const nextConfig: NextConfig = {
     /* config options here */
     basePath: "/app",
     // distDir: "build",
     output: "standalone",
+    // assetPrefix: isDev ? undefined : '/app/',
     eslint: {
         ignoreDuringBuilds: true,
     },
